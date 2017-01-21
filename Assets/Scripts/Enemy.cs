652284a15b8264b7ff2bffa022b287012e16bc30
Charles Williams <crw4147@rit.edu>
2017-01-20 21:01:06 -0500

    Charles init push

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    /// <summary>
+    /// responsible for seeking the enemy's target.
+    /// </summary>
+    public Vector3 Seek()
+    {
+        Vector3 offset = target.transform.position - this.transform.position;
+        Vector3 unitOffset = offset.normalized;
+        return unitOffset;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
