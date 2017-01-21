f3d33f6320269fe9fd530d4b961e8afbcaae36b5
Kevin Idzik <kdi4293@rit.edu>
2017-01-21 00:04:50 -0500

    Small Movement Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,27 +30,55 @@ public class Player : MonoBehaviour
 
     void Move() //Player movement
     {
-        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * playerRotationSpeed, 0); //Rotate the player
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * playerRotationSpeed, 0); //Rotate the player
+        //
+        //if (Mathf.Abs(Input.GetAxis("Vertical")) > .05) //If the player should move
+        //{
+        //    movement += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed * 3);
+        //}
+        //else //If the player should stop
+        //{
+        //    movement = Vector3.zero;
+        //}
+        //
+        //if (movement.z >= playerMoveSpeed) //If the player is moving too fast
+        //{
+        //    movement.z = playerMoveSpeed;
+        //}
+        //else if (movement.z < -playerMoveSpeed) //If the player is moving too fast
+        //{
+        //    movement.z = -playerMoveSpeed;
+        //}
+        //
+        //transform.Translate(movement); //Move the player
 
-        if (Mathf.Abs(Input.GetAxis("Vertical")) > .05) //If the player should move
-        {
-            movement += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed * 3);
-        }
-        else //If the player should stop
-        {
-            movement = Vector3.zero;
-        }
 
-        if (movement.z >= playerMoveSpeed) //If the player is moving too fast
-        {
-            movement.z = playerMoveSpeed;
-        }
-        else if (movement.z < -playerMoveSpeed) //If the player is moving too fast
-        {
-            movement.z = -playerMoveSpeed;
-        }
 
-        transform.Translate(movement); //Move the player
+        Debug.DrawLine(transform.position, Input.mousePosition, Color.red);
+        
+        //transform.LookAt(new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y)); //Rotate the player
+
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward, Input.mousePosition); //Rotate the player
+
+        //if (Mathf.Abs(Input.GetAxis("Vertical")) > .05) //If the player should move
+        //{
+        //    movement += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed * 3);
+        //}
+        //else //If the player should stop
+        //{
+        //    movement = Vector3.zero;
+        //}
+        //
+        //if (movement.z >= playerMoveSpeed) //If the player is moving too fast
+        //{
+        //    movement.z = playerMoveSpeed;
+        //}
+        //else if (movement.z < -playerMoveSpeed) //If the player is moving too fast
+        //{
+        //    movement.z = -playerMoveSpeed;
+        //}
+        //
+        //transform.Translate(movement); //Move the player
     }
 
     void Ping() //Player ping
