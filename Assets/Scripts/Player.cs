0d1e89c4adef91bd8b646b8eb0f54ec609e2b739
Kevin Idzik <kdi4293@rit.edu>
2017-01-20 23:13:30 -0500

    Basic Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //Attributes
+    private Rigidbody playerRB;
+    [SerializeField] private float playerRotationSpeed;
+    [SerializeField] private float playerMoveSpeed;
+    private Vector3 movement = Vector3.zero;
 
     //Properties
 
 
     void Start() //Use this for initialization
     {
-
+        playerRB = GetComponent<Rigidbody>(); //Get the player's rigidbody
     }
 
     void Update() //Update is called once per frame
@@ -26,7 +30,27 @@ public class Player : MonoBehaviour
 
     void Move() //Player movement
     {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * playerRotationSpeed, 0); //Rotate the player
 
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > .05) //If the player should move
+        {
+            movement += new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed * 3);
+        }
+        else //If the player should stop
+        {
+            movement = Vector3.zero;
+        }
+
+        if (movement.z >= playerMoveSpeed) //If the player is moving too fast
+        {
+            movement.z = playerMoveSpeed;
+        }
+        else if (movement.z < -playerMoveSpeed) //If the player is moving too fast
+        {
+            movement.z = -playerMoveSpeed;
+        }
+
+        transform.Translate(movement); //Move the player
     }
 
     void Ping() //Player ping
