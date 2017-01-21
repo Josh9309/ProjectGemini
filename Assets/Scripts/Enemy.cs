0460a3439e6d010cd7313be65f3cd2d3cb02daf3
Charles Williams <crw4147@rit.edu>
2017-01-21 14:13:18 -0500

    debug push
    DONT USE ENEMIES YET

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,11 +11,24 @@ public class Enemy : MonoBehaviour {
     private float visionAngle;
     private NavMeshAgent agent;
     private DetectionSphere detectionSphere;
+    [SerializeField]
+    private List<GameObject> patrolRoute;
+    private List<bool> visitedWaypoints;
+    private GameObject currentWaypoint;
 
     // Use this for initialization
     void Start () {
         agent = GetComponent<NavMeshAgent>();
         detectionSphere = GetComponentInChildren<DetectionSphere>();
+        Debug.Log(patrolRoute.Count);
+
+        visitedWaypoints = new List<bool>();
+
+        // assign a default value of false for each waypoint in the patrol route
+        for (int i = 0; i < patrolRoute.Count; i++)
+        {
+            visitedWaypoints.Add(false);
+        }
 	}
 
     /// <summary>
@@ -40,7 +53,7 @@ public class Enemy : MonoBehaviour {
 
         float angle = Vector3.Angle(direction, transform.forward);
 
-        Debug.Log(angle);
+        //Debug.Log(angle);
 
         //Debug.DrawLine(transform.position, transform.position + transform.forward.normalized * 1.5f);
         Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(visionAngle, transform.up) * transform.forward * 5);
@@ -69,11 +82,63 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// routes the enemy along the patrol route,
+    /// hitting each waypoint before resetting the
+    /// route
+    /// </summary>
+    private void Patrol()
+    {
+        for (int i = 0; i < patrolRoute.Count; i++)
+        {
+            if (visitedWaypoints[i] == false)
+            {
+                while (visitedWaypoints[i] == false)
+                {
+                    agent.destination = patrolRoute[i].transform.position;
+
+                    currentWaypoint = patrolRoute[i];
+                }
+
+                if (currentWaypoint == patrolRoute[patrolRoute.Count])
+                {
+                    for (int j = 0; j < visitedWaypoints.Count; j++)
+                    {
+                        visitedWaypoints[i] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// dectecting when an enemy hits a waypoint
+    /// </summary>
+    /// <param name="coll"></param>
+    public void OnTriggerEnter(Collider coll)
+    {
+        if (coll.transform.tag == "Waypoint")
+        {
+            for (int i = 0; i < visitedWaypoints.Count; i++)
+            {
+                if (visitedWaypoints[i] == false)
+                {
+                    visitedWaypoints[i] = true;
+                    return;
+                }
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update () {
 		if (VisionCone() || detectionSphere.PlayerDetected)
         {
             agent.destination = target.transform.position;
+        }
+        else
+        {
+            Patrol();
         }
 	}
 }
