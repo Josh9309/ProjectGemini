8dcd17f736d0ef03004361cf27a4a8eae20633e2
Kevin Idzik <kdi4293@rit.edu>
2017-01-21 11:30:38 -0500

    Beginning Color Change UI

--- Assets/Scripts/ColorMenu.cs
+++ Assets/Scripts/ColorMenu.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorMenu : MonoBehaviour {
+public class ColorMenu : MonoBehaviour
+{
+    //[SerializeField] private GameObject m_Player; //The player for the inspector
+    //[SerializeField] private GameObject m_Enemy; //The enemy for the inspector
+    ////private MeshFilter playerModel; //The player's model
+    //private Renderer playerTextureRenderer; //The player's texture renderer
+    ////private MeshFilter enemyModel; //The enemy's Model
+    //private Renderer enemyTextureRenderer; //The enemy's texture renderer
 
-	// Use this for initialization
-	void Start () {
-		
+    //[SerializeField] private Material playerMaterial; //The player's material
+    //[SerializeField] private Material enemyMaterial; //The enemy's material
+
+    [SerializeField] private Renderer playerTextureRenderer; //The player's texture renderer
+    [SerializeField] private Renderer enemyTextureRenderer; //The enemy's texture renderer
+
+    //[SerializeField] private Texture playerTexture; //The player's texture
+    //[SerializeField] private Texture enemyTexture; //The enemy's texture
+
+    void Start() //Use this for initialization
+    {
+        //playerModel = m_Player.GetComponent<MeshFilter>(); //Get the player's model
+        //playerTextureRenderer = m_Player.GetComponent<Renderer>(); //Get the player's texture renderer
+        //enemyModel = m_Enemy.GetComponent<MeshFilter>(); //Get the enemy's model
+        //enemyTextureRenderer = m_Enemy.GetComponent<Renderer>(); //Get the enemy's texture renderer
+
+        enemyTextureRenderer.material.SetFloat("_Mode", 0.0f); //Change the rendering mode of the enemy
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update() //Update is called once per frame
+    {
 		
 	}
 }
