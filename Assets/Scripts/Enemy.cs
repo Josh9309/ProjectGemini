ca70e5724874b8bf2d48dcc7c16346b339c0e610
Charles Williams <crw4147@rit.edu>
2017-01-21 00:05:15 -0500

    Vision Cone Yo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,12 +8,15 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     private GameObject target;
     [SerializeField]
-    float maxSpeed;
+    private float visionAngle;
+    [SerializeField]
+    private float objectRadius;
+    private NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start () {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = target.transform.position;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = target.transform.position;
 	}
 
     /// <summary>
@@ -26,8 +29,48 @@ public class Enemy : MonoBehaviour {
         return unitOffset;
     }
 
+    /// <summary>
+    /// responsible for detecting the player based on
+    /// the cone of vision and checking 
+    /// </summary>
+    private bool VisionCone()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = target.transform.position - this.transform.position;
+
+        float angle = Vector3.Angle(direction, transform.forward);
+
+        if (angle < 90 - visionAngle || angle > 90 + visionAngle)
+        {
+            Debug.Log(angle);
+
+            return false;
+        }
+        else
+        {
+            RaycastHit hit;
+
+            Physics.Raycast(transform.position, direction, out hit);
+
+            Debug.Log(hit.transform.gameObject.name);
+
+            if (hit.transform.gameObject == target)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update () {
-		
+		if (VisionCone())
+        {
+            agent.destination = target.transform.position;
+        }
 	}
 }
